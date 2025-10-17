28330694f1013231cf18059a14181420e96a89d3
IvanDragonDeath <forthmankayden@gmail.com>
2025-10-16 18:11:46 -0600

    Added the database, repository, Dao, and i separated the view model and course data class from the mainActivity.kt to more explicitly follow MVVM. I used chat gpt to help set up KSP and Room correctly. I also used it to debug error messages whenever they popped up when i tried to build.

--- Assignment22/app/src/main/java/com/example/assignment2/MainActivity.kt
+++ Assignment22/app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
